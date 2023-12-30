262bb369a8013a2bcf967802763d44be9659c8ad
Sumaiya <sumaiya.16.ete.cuet@gmail.com>
2023-12-30 09:46:29 +0600

    Social_login

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,1 @@
+<a href ="{{ route('login')  }}"><h4>login with google</h4></a>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GoogleController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('auth/google',[GoogleController::class,'loginWithGoogle'])->name('login');
+Route::any('auth/google/callback',[GoogleController::class,'callbackFromGoogle'])->name('callback');
+Route::get('home',function(){
+    return view('home');
+})->name('home');
+
